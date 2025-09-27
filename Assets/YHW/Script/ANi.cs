48770ffa1d23fac3c87ce803e29b40745c95d5d6
MM <manualmaker89@gmail.com>
2025-09-28 01:38:44 +0900

    조립 보강

--- Assets/YHW/Script/ANi.cs
+++ Assets/YHW/Script/ANi.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class ANi : MonoBehaviour
 {
     GameObject capturedImage;
+    GameObject groupresultfail;
 
     private void Awake()
     {
         capturedImage = GameObject.Find("CapturedImgae");
+        groupresultfail = GameObject.Find("Group_Result_Fail");
 
     }
 
     public void ABCDEFFF()
     {
         capturedImage.SetActive(false);
+        groupresultfail.SetActive(false);
     }
 }
